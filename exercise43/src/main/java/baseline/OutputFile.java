38eb1f7547105ed4725ad90fe8105fc80f5f604b
Praween Pongpat <praween.pongpat@gmail.com>
2021-10-15 12:46:40 -0400

    UML and test codes

--- exercise43/src/main/java/baseline/OutputFile.java
+++ exercise43/src/main/java/baseline/OutputFile.java
@@ -1,38 +1,60 @@
 package baseline;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public record OutputFile(String nameOfSite, String author, boolean isJS, boolean isCSS) {
 
     //method to manage to create folder/files as needed from the given name
-    private void fileFolderManager() {
+    public void fileFolderManager() {
         //create folder based on nameOfSite: data--->nameOfSite
-
+        File baseFolder = new File("data\\website\\"+nameOfSite);
+        if(baseFolder.mkdirs())
+            reportFolderFileExisted(baseFolder);
         //call method makeHTMLFile to create .html with proper data init
+        File htmlFile = new File(baseFolder+"\\index.html");
+        makeHTMLFile(baseFolder.toString());
+        reportFolderFileExisted(htmlFile);
 
         //if isJS is true, create JS folder: data--->nameOfSite--->JS
+        File jsFolder = new File(baseFolder+"\\JS");
+        if(isJS && jsFolder.mkdirs())
+            reportFolderFileExisted(jsFolder);
 
         //if isCSS is true, create CSS folder: data--->nameOfSite--->CSS
-
-        //call method reportFolderFileExisted passing Files object to check
+        File cssFolder = new File(baseFolder+"\\CSS");
+        if(isCSS && cssFolder.mkdirs())
+            reportFolderFileExisted(cssFolder);
     }
 
     //create an HTML file uses the information of nameOfSite in <title> and author name in <meta>
-    private void makeHTMLFile() {
+    private void makeHTMLFile(String basePath) {
         //create HTML file
+        File output = new File(basePath+ "\\index.html");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(output));
 
-        //create content in HTML file
-        //write <title>nameOfSite</title> to it
-        //write <meta> with author's name in it
-        //close writer
+            //create content in HTML file
+            //write <title>nameOfSite</title> to it
+            writer.write("<title>"+nameOfSite+"</title>");
+            //write <meta> with author's name in it
+            writer.write("<meta name=\"description\" content=\""+author+"\">");
+            //close writer
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("HTML files cannot be made...");
+        }
     }
 
-    private void reportFolderFileExisted (File fileName) {
-
+    private void reportFolderFileExisted (File file) {
         //check if the file/folder exists
-        //if so,    display "created."
-        //          display the relative path of the file/folder
-
+        if(file.exists()) {
+            //if so,    display "created."
+            //          display the relative path of the file/folder
+            System.out.println("created. "+file.getPath());
+        }
     }
 
 }
---
+++ exercise43/src/test/java/baseline/OutputFileTest.java
@@ -0,0 +1,71 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OutputFileTest {
+    @Test
+    void fileFolderManagerTest1() {
+        OutputFile createOnlyHTML = new OutputFile("awesomeco","james",false,false);
+        createOnlyHTML.fileFolderManager();
+
+        File baseFolderTest1 = new File("data\\website\\awesomeco");
+        boolean resBaseFolder1 = baseFolderTest1.exists();
+
+        File htmlFileTest1 = new File("data\\website\\awesomeco\\index.html");
+        boolean resHtmlFile1 = htmlFileTest1.exists();
+
+        File jsFolderTest1 = new File("data\\website\\awesomeco\\JS");
+        boolean resJsFile1 = jsFolderTest1.exists();
+
+        File cssFolderTest1 = new File("data\\website\\awesomeco\\CSS");
+        boolean resCssFile1 = cssFolderTest1.exists();
+
+        assertEquals(true,resBaseFolder1);
+        assertEquals(true,resHtmlFile1);
+        assertEquals(false,resJsFile1);
+        assertEquals(false,resCssFile1);
+
+        //manually delete directories for other tests
+        htmlFileTest1.delete();
+        baseFolderTest1.delete();
+        File dataWebsiteDir = new File("data\\website");
+        dataWebsiteDir.delete();
+        //at this point of finishing test1, leaving only blank dir 'data'
+    }
+    @Test
+    void fileFolderManagerTest2() {
+        //testing if files/folders exists
+        OutputFile createAll = new OutputFile("awesomeco","james",true,true);
+        createAll.fileFolderManager();
+
+        File baseFolderTest2 = new File("data\\website\\awesomeco");
+        boolean resBaseFolder2 = baseFolderTest2.exists();
+
+        File htmlFileTest2 = new File("data\\website\\awesomeco\\index.html");
+        boolean resHtmlFile2 = htmlFileTest2.exists();
+
+        File jsFolderTest2 = new File("data\\website\\awesomeco\\JS");
+        boolean resJsFile2 = jsFolderTest2.exists();
+
+        File cssFolderTest2 = new File("data\\website\\awesomeco\\CSS");
+        boolean resCssFile2 = cssFolderTest2.exists();
+
+        assertEquals(true,resBaseFolder2);
+        assertEquals(true,resHtmlFile2);
+        assertEquals(true,resJsFile2);
+        assertEquals(true,resCssFile2);
+
+        //manually delete directories for other tests
+        htmlFileTest2.delete();
+        jsFolderTest2.delete();
+        cssFolderTest2.delete();
+        baseFolderTest2.delete();
+        File dataWebsiteDir = new File("data\\website");
+        dataWebsiteDir.delete();
+        //at this point of finishing test1, leaving only blank dir 'data'
+    }
+}
